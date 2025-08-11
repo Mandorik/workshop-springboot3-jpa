13938cc27689023b76de7ef557428fa1eec4fab1
Mandorik <arthur_jakobi@hotmail.com>
2025-08-11 14:40:01 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -22,7 +22,6 @@ public class UserResource {
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
-				
 		return ResponseEntity.ok().body(list);
 	}
 	
